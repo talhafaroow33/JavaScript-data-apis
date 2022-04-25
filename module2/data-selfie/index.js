0e1524eb30a6533160bfdaea56c4738bae8b7ecb
talhafaroow33 <69416703+talhafaroow33@users.noreply.github.com>
2022-04-25 18:14:16 +0500

    added button to send post request in data selfie app

--- module2/data-selfie/index.js
+++ module2/data-selfie/index.js
@@ -5,3 +5,26 @@ app.listen(3000, () => {
   console.log("listening on port 3000");
 });
 app.use(express.static("public"));
+app.use(express.json({ limit: "1mb" }));
+
+const allData = [];
+
+app.post("/api/", (require, response) => {
+  console.log(require.body);
+  const data = require.body;
+  allData.push(data);
+  response.json(allData);
+  console.log(allData);
+});
+
+// Making a simple request to the server
+
+// app.post("/api/", (require, response) => {
+//   console.log(require.body);
+//   const { latitude, longitude } = require.body;
+//   response.json({
+//     status: "success",
+//     latitude: latitude,
+//     longitude: longitude,
+//   });
+// });
--- module2/data-selfie/public/script/index.js
+++ module2/data-selfie/public/script/index.js
@@ -1,11 +1,31 @@
 if ("geolocation" in navigator) {
   console.log("geolocation is available");
-  navigator.geolocation.getCurrentPosition((position) => {
+  navigator.geolocation.getCurrentPosition(async (position) => {
     const { longitude, latitude } = position.coords;
 
     document.getElementById("lat").innerHTML = latitude;
     document.getElementById("lon").innerHTML = longitude;
+    const btn = document.getElementById("btn");
+
+    btn.addEventListener("click", () => {
+      sendGeolocation(latitude, longitude);
+    });
   });
 } else {
   console.log("geolocation is not available");
+}
+
+async function sendGeolocation(latitude, longitude) {
+  const data = { latitude, longitude };
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  };
+
+  const response = await fetch("/api", options);
+  const responseData = await response.json();
+  console.log("<<<<<<response", responseData);
 }
